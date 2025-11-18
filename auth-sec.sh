a3e67346fbd7ba145e0c5cf6b00674e8e689f163
Evan Hoefling <evanh1393@gmail.com>
2025-11-17 20:38:09 -0500

    Hello Changes

--- auth-sec.sh
+++ auth-sec.sh
@@ -20,25 +20,26 @@ DESTINATION_PRIVATE_KEY_PATH="~/.ssh/$KEY_NAME"
 scp "$PRIVATE_KEY_PATH" "$DESTINATION_SSH":"$DESTINATION_PRIVATE_KEY_PATH"
 ssh "$DESTINATION_SSH" "chmod 600 $DESTINATION_PRIVATE_KEY_PATH"
 ssh -o StrictHostKeyChecking=no "$DESTINATION_SSH" "scp -o 'StrictHostKeyChecking=no' -i $DESTINATION_PRIVATE_KEY_PATH forge@$TARGET_IP:~/test.txt /home/forge/test_from_target.txt"
-
+# Due to the danger of potentially deleting everything, in sensitive folders best delete things yourself.
+# otherwise feel free to uncomment...
+#ssh "$DESTINATION_SSH" "rm -rf $DESTINATION_FOLDER_PATH/*"
 ssh "$DESTINATION_SSH" "mkdir -p $DESTINATION_FOLDER_PATH"
 
 CURRENT_MONTH=$(date +"%Y/%m")
 LAST_MONTH=$(date -d "1 month ago" +"%Y/%m")
 TWO_MONTHS_AGO=$(date -d "2 months ago" +"%Y/%m")
+TARGET_YEAR=$(date +"%Y")
 
 ssh -o StrictHostKeyChecking=no "$DESTINATION_SSH" \
-  "rsync -avz -e --progress 'ssh -o StrictHostKeyChecking=no -i $DESTINATION_PRIVATE_KEY_PATH' \
-  $TARGET_USER@$TARGET_IP:$TARGET_FOLDER_PATH/$CURRENT_MONTH \
-  $TARGET_USER@$TARGET_IP:$TARGET_FOLDER_PATH/$LAST_MONTH \
-  $TARGET_USER@$TARGET_IP:$TARGET_FOLDER_PATH/$TWO_MONTHS_AGO \
-  $DESTINATION_FOLDER_PATH"
-
-rm -f "$PRIVATE_KEY_PATH" "$PUBLIC_KEY_PATH"
-ssh "$DESTINATION_SSH" "rm -f $DESTINATION_PRIVATE_KEY_PATH"
+"rsync -avz --progress -e 'ssh -o StrictHostKeyChecking=no -i $DESTINATION_PRIVATE_KEY_PATH' \
+  $TARGET_USER@$TARGET_IP:$TARGET_FOLDER_PATH/{$CURRENT_MONTH,$LAST_MONTH,$TWO_MONTHS_AGO} \
+  $DESTINATION_FOLDER_PATH/$TARGET_YEAR/"
 
 KEY_STRING=$(awk '{print $2}' "$PUBLIC_KEY_PATH")
 ssh -o StrictHostKeyChecking=no "$TARGET_SSH" "sed -i '/$KEY_STRING/d' ~/.ssh/authorized_keys"
+
+rm -f "$PRIVATE_KEY_PATH" "$PUBLIC_KEY_PATH"
+ssh "$DESTINATION_SSH" "rm -f $DESTINATION_PRIVATE_KEY_PATH"
 
 aws ec2 revoke-security-group-ingress \
   --group-id "$TARGET_SECURITY_GROUP_ID" \
